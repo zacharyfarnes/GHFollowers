a4bd01c831f1ae169a589e296e007347bc77c85c
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-09 22:34:07 +0000

    Create models and fix minor UI bugs

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Zachary Farnes on 09/01/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
