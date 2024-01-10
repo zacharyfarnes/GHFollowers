8a4df8271d68cca3667b1a5878e73b1998876134
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-10 21:40:05 +0000

    Navigate to user info view from follower list

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Zachary Farnes on 10/01/2024.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
