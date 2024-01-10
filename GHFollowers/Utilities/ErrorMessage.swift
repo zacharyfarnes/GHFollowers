b5a60275d50c28412af4fa838573f3f940c1f7cd
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-10 11:44:17 +0000

    Refactor error messages and fix minor bug

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Zachary Farnes on 10/01/2024.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
