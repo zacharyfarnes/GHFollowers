7cea09f38713b9532e64a418752d153da1a2a924
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-10 20:32:26 +0000

    Add empty state view and reorganise files

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -60,6 +60,14 @@ class FollowerListVC: UIViewController {
                     self.hasMoreFollowers = false
                 }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
+                }
                 self.updateData()
                 
             case .failure(let error):
