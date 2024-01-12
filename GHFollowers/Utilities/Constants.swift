e9caa5ac5be6b426ca544e1112b12a33fea3da2c
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-12 16:27:26 +0000

    Add header view to user info screen

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Zachary Farnes on 12/01/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
