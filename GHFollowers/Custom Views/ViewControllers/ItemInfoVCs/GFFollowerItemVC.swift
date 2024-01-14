ff1b8b1fe5347ac6668b634b9c3a4589e4261e4c
Zachary Farnes <zachary.farnes@gmail.com>
2024-01-14 14:49:49 +0000

    Add repo and follower items to user info screen

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Zachary Farnes on 14/01/2024.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
+
